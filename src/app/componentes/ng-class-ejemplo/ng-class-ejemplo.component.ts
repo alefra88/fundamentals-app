45fbaae1e1f3f15237524e4ac9d3b044c34e328c
alefra88 <efra.rlz@gmail.com>
2025-02-13 03:37:33 -0600

    formularios reactivos

--- src/app/componentes/ng-class-ejemplo/ng-class-ejemplo.component.ts
+++ src/app/componentes/ng-class-ejemplo/ng-class-ejemplo.component.ts
@@ -1,10 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ng-class-ejemplo',
   imports: [
-    NgClass
+    NgClass,
+    ReactiveFormsModule
   ],
   templateUrl: './ng-class-ejemplo.component.html',
   styleUrl: './ng-class-ejemplo.component.css'
@@ -14,5 +16,26 @@ export class NgClassEjemploComponent {
 
   toggleDark(){
     this.isDark = !this.isDark
+  }
+
+  //formulario
+  movieForm: FormGroup;
+  name:FormControl;
+  duration: FormControl;
+  director: FormControl;
+
+  constructor(){
+    this.name = new FormControl('');
+    this.duration =  new FormControl('');
+    this.director = new FormControl('');
+
+    this.movieForm = new FormGroup({
+      name: this.name,
+      duration: this.duration,
+      director: this.director
+    })
+  }
+  handleSubmit(): void {
+    console.log(this.movieForm);
   }
 }
